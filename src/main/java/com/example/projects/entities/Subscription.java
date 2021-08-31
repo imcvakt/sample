8366f7ba03e67bacecb691b923de0d854d055a04
iorran castro <iorran.castro@vakt.io>
2021-08-31 04:25:25 +0100

    test

--- src/main/java/com/example/projects/entities/Subscription.java
+++ src/main/java/com/example/projects/entities/Subscription.java
@@ -16,15 +16,6 @@ public class Subscription {
     @Column
     private Long id;
 
-    // nowadays with channel generic, we have only channels mapped into subscriptions
-
-    /*
-
-    A simplest approach would be just by accepting nullable fields.
-    Ifs are necessary to identify the channel
-
-     */
-
     @ManyToOne
     @JoinColumn(name = "teamsId")
     private Teams teams;
@@ -32,12 +23,6 @@ public class Subscription {
     @ManyToOne
     @JoinColumn(name = "webPushId")
     private WebPush webPush;
-
-    // To avoid nullable fields
-    // we could add a TYPE column to define which channel is that id
-
-    //@Column
-    //private Long channelId;
 
     @Column
     @Enumerated(EnumType.STRING)
